17939bfdbb216cbb3feaeefe9c5ce304af555f06
billydehaochen <dehaochenbilly@gmail.com>
2024-05-21 20:26:48 -0400

    push

--- R/main.sh
+++ R/main.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sbatch -p preemptable --export=ALL, network="Rural", layer="School", est_apch="mcmle", percent_target_pop="0.4", runest.sh
+sbatch -p preemptable --export=ALL,network="Rural",layer="School",est_apch="mcmle",percent_target_pop="0.4" runest.sh
