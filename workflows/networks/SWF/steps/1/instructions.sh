38f46c97a9f6d3b8f6e4aab2b5389da0141c267a
billydehaochen <dehaochenbilly@gmail.com>
2024-06-12 12:21:40 -0400

    push

--- workflows/networks/SWF/steps/1/instructions.sh
+++ workflows/networks/SWF/steps/1/instructions.sh
@@ -2,8 +2,8 @@
 spack unload -a
 spack load r@4.3.2
 CUR_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-if [[ "$CUR_BRANCH" != "new_ctrl" ]]; then
-echo 'The git branch is not `new_ctrl`.)
+if [[ "$CUR_BRANCH" != "main" ]]; then
+echo 'The git branch is not `main`.)
 Exiting' 1>&2
 exit 1
 fi
