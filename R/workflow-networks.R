620dca06c8c91cb19f5ae0d2c50f4361750bd129
billydehaochen <dehaochenbilly@gmail.com>
2024-06-17 11:09:52 -0400

    Update workflow-networks.R

--- R/workflow-networks.R
+++ R/workflow-networks.R
@@ -15,43 +15,43 @@ source("R/hpc_configs.R", local = TRUE)
 # Process ----------------------------------------------------------------------
 wf <- make_em_workflow("networks", override = TRUE)
 
-# netest
-wf <- add_workflow_step(
-  wf_summary = wf,
-  step_tmpl = step_tmpl_map_script(
-    r_script = "R/3-network_est.R",
-    layer=c("Home","School","Work","Nonhome"),
-    args = list(hpc_context = TRUE, 
-                network="Urban",
-                est_apch="mcmle",
-                percent_target_pop="0.4"),
-    setup_lines = hpc_node_setup
-  ),
-  sbatch_opts = list(
-    "cpus-per-task" = est_cores,
-    "time" = "24:00:00",
-    "mem" = "0"
-  )
-)
-
-# netdx
-wf <- add_workflow_step(
-  wf_summary = wf,
-  step_tmpl = step_tmpl_map_script(
-    r_script = "R/4-network_dx.R",
-    args = list(hpc_context = TRUE,
-                network="Rural",
-                layer="Home",
-                est_apch="mcmle",
-                percent_target_pop="0.4"),
-    setup_lines = hpc_node_setup
-  ),
-  sbatch_opts = list(
-    "cpus-per-task" = est_cores,
-    "time" = "24:00:00",
-    "mem" = "0"
-  )
-)
+# # netest
+# wf <- add_workflow_step(
+#   wf_summary = wf,
+#   step_tmpl = step_tmpl_map_script(
+#     r_script = "R/3-network_est.R",
+#     layer=c("Home","School","Work","Nonhome"),
+#     args = list(hpc_context = TRUE, 
+#                 network="Urban",
+#                 est_apch="mcmle",
+#                 percent_target_pop="0.4"),
+#     setup_lines = hpc_node_setup
+#   ),
+#   sbatch_opts = list(
+#     "cpus-per-task" = est_cores,
+#     "time" = "24:00:00",
+#     "mem" = "0"
+#   )
+# )
+# 
+# # netdx
+# wf <- add_workflow_step(
+#   wf_summary = wf,
+#   step_tmpl = step_tmpl_map_script(
+#     r_script = "R/4-network_dx.R",
+#     args = list(hpc_context = TRUE,
+#                 network="Rural",
+#                 layer="Home",
+#                 est_apch="mcmle",
+#                 percent_target_pop="0.4"),
+#     setup_lines = hpc_node_setup
+#   ),
+#   sbatch_opts = list(
+#     "cpus-per-task" = est_cores,
+#     "time" = "24:00:00",
+#     "mem" = "0"
+#   )
+# )
 
 # netsim
 wf <- add_workflow_step(
