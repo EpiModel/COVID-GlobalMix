fe7209b72c28f8e906a483295ed716759131eb05
billydehaochen <dehaochenbilly@gmail.com>
2024-06-17 12:14:40 -0400

    push

--- R/utils-hpc_configs.R
+++
@@ -1,7 +0,0 @@
-# Must be sourced **AFTER** "./R/utils-0_project_settings.R"
-
-hpc_configs <- EpiModelHPC::swf_configs_rsph(
-  partition = "epimodel",
-  r_version = "4.3.1",
-  mail_user = "dehao.chen@emory.edu"
-)
--- R/workflow-netsim.R
+++ R/workflow-netsim.R
@@ -16,6 +16,24 @@ source("R/hpc_configs.R", local = TRUE)
 wf <- make_em_workflow("net_est_dx", override = TRUE)
 
 # netest, using Map function.
+# wf <- add_workflow_step(
+#   wf_summary = wf,
+#   step_tmpl = step_tmpl_map_script(
+#     r_script = "R/3-network_est.R",
+#     layer=c("Home","School","Work","Nonhome"),
+#     args = list(hpc_context = TRUE,
+#                 network="Urban",
+#                 est_apch="mcmle",
+#                 percent_target_pop="0.4"),
+#     setup_lines = hpc_node_setup
+#   ),
+#   sbatch_opts = list(
+#     "cpus-per-task" = est_cores,
+#     "time" = "24:00:00",
+#     "mem" = "0"
+#   )
+# )
+
 wf <- add_workflow_step(
   wf_summary = wf,
   step_tmpl = step_tmpl_map_script(
@@ -33,6 +51,8 @@ wf <- add_workflow_step(
     "mem" = "0"
   )
 )
+
+
 # 
 # # netdx
 # wf <- add_workflow_step(
