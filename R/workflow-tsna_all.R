b649e6e5c46573432dcd9004c29e733640b34663
billydehaochen <dehaochenbilly@gmail.com>
2024-06-25 09:40:30 -0400

    push

--- R/workflow-tsna_all.R
+++ R/workflow-tsna_all.R
@@ -16,13 +16,13 @@ wf <- make_em_workflow("tsna_all", override = TRUE)
 
 wf <- add_workflow_step(
   wf_summary = wf,
-  step_tmpl = step_tmpl_map_script(
+  step_tmpl = step_tmpl_do_call_script(
     r_script = "R/6-tsna.R",
-    layer=c("Home","School","Work","Nonhome"),
-    MoreArgs = list(hpc_context = TRUE,
+    args = list(hpc_context = TRUE,
+                layer = "All",
                 network="Urban",
                 est_apch="mcmle",
-                percent_target_pop="0.4"),
+                percent_target_pop="0.1"),
     setup_lines = hpc_node_setup
   ),
   sbatch_opts = list(
@@ -31,24 +31,6 @@ wf <- add_workflow_step(
     "mem" = "0"
   )
 )
-
-# wf <- add_workflow_step(
-#   wf_summary = wf,
-#   step_tmpl = step_tmpl_do_call_script(
-#     r_script = "R/6-tsna.R",
-#     args = list(hpc_context = TRUE,
-#                 layer = "School",
-#                 network="Urban",
-#                 est_apch="mcmle",
-#                 percent_target_pop="0.1"),
-#     setup_lines = hpc_node_setup
-#   ),
-#   sbatch_opts = list(
-#     "cpus-per-task" = est_cores,
-#     "time" = "24:00:00",
-#     "mem" = "0"
-#   )
-# )
 
 
 
