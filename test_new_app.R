f39edfe4dafdcf364be4124d5a50b8baa525ac66
Adrien Le Guillou <git@aleguillou.org>
2024-07-05 11:32:43 +0200

    ups

--- test_new_app.R
+++ test_new_app.R
@@ -12,76 +12,33 @@ el_cuml <- readRDS("el_cuml__non.rds")
 # el_cuml <- readRDS("./el_cuml_tom.rds")
 
 n_nodes <- max(el_cuml$head, el_cuml$tail)
-nodes <- sample(n_nodes, 1e3)
+nodes <- sample(n_nodes, 1e2)
+
+options("browser" = "firefox")
+profvis::profvis({
+  progressr::with_progress(
+    x <- get_all_frp( el_cuml, 1, 52, nodes)
+  )
+})
+
+system.time({
+    x <- get_all_frp( el_cuml, 1, 52, nodes)
+})
 
 # start <- Sys.time()
 # progressr::with_progress(
-#  frps <- get_all_frp(
-#    el_cuml, 1, 260,
+#  frps_old <- old_get_frp_lengths(
+#    el_cuml, 1, 52,
 #    nodes
 #  )
 # )
 # print(Sys.time() - start)
-
-start <- Sys.time()
-progressr::with_progress(
- frps_blk <- get_all_frp_block(
-   el_cuml, 1, 26,
-   nodes
- )
-)
-print(Sys.time() - start)
-
-
-
-
-start <- Sys.time()
-progressr::with_progress(
- frp_lengths_sub <- get_frp_lengths_sub(
-   el_cuml, 1, 260,
-   nodes
- )
-)
-frp_lengths_sub
-print(Sys.time() - start)
-
-for (i in seq_along(frp_lengths)) {
-  if (frp_lengths[i] != frp_lengths_sub[i]) {
-    print(i)
-  }
-}
-
-cur_el
-
-new_get_subnets_el <- function(connected, el) {
-  subnets <- list()
-  con_el <- get_connected(el, length(connected))
-  j <- 1
-  for (i in seq_along(con_el)) {
-    if (length(con_el[[i]]) != 0) {
-      subnets[[j]] <- get_subnet(connected, con_el[[i]])
-      con_el[ subnets[[j]] ] <- list(NULL)
-      j <- j + 1
-    }
-  }
-  subnets
-}
-
-# get the network components involving some edges
-get_subnets_el <- function(connected, el) {
-  subnets <- list()
-  i <- 1
-  while (nrow(el) > 0) {
-    subnets[[i]] <- get_subnet(connected, el$head[[1]]) # by def: head <-> tail
-    # TODO: speed up with a `connected` approach?
-    el <- dplyr::filter(el, !head %in% subnets[[i]])
-    i <- i + 1
-  }
-  subnets
-}
-
-connected <- get_connected(full_el, n_nodes)
-microbenchmark::microbenchmark(
-  new = new_get_subnets_el(connected, cur_el),
-  old = get_subnets_el(connected, cur_el)
-)
+#
+# start <- Sys.time()
+# progressr::with_progress(
+#  frps_len <- get_frp_lengths(
+#    el_cuml, 1, 52,
+#    nodes
+#  )
+# )
+# print(Sys.time() - start)
