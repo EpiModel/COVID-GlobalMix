f66139ad624e73a5aeac5b0975423cff2b90a7d9
billydehaochen <dehaochenbilly@gmail.com>
2024-07-08 20:13:39 -0400

    Update workflow-netest_dx.R

--- R/workflow-netest_dx.R
+++ R/workflow-netest_dx.R
@@ -13,7 +13,7 @@ hpc_context <- TRUE
 source("R/hpc_configs.R", local = TRUE)
 
 # Process ----------------------------------------------------------------------
-wf <- make_em_workflow("netest_dx", override = TRUE)
+wf <- make_em_workflow("netest_dx_0.1", override = TRUE)
 
 # netest
 wf <- add_workflow_step(
