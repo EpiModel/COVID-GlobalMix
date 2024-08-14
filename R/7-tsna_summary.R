9ac9016dfc1c5559e976ed57382411a83a7af64a
billydehaochen <dehaochenbilly@gmail.com>
2024-08-14 14:23:34 -0400

    push

--- R/7-tsna_summary.R
+++ R/7-tsna_summary.R
@@ -1,4 +1,4 @@
-library(dplyr); library(tidyr); library(tibble); library(ggplot2); library(ggpubr);
+library(dplyr); library(tidyr); library(tibble); library(ggplot2); library(ggpubr); library(kableExtra)
 # Note: the purpose of this script is to summarize result the FRP calculation. 
 # The following are the analytical scenarios
 network = c("Rural", "Urban")
@@ -42,12 +42,12 @@ frp_nh_r <- readRDS(file.name_r[5])
  
 # frp_all_u <- readRDS(file.name_u[1])
 # frp_h_u <- readRDS(file.name_u[2])
-# frp_s_u <- readRDS(file.name_u[3])
-# frp_w_u <- readRDS(file.name_u[4])
+frp_s_u <- readRDS(file.name_u[3])
+frp_w_u <- readRDS(file.name_u[4])
 # frp_nh_u <- readRDS(file.name_u[5])
 
 
-# Process nodal attribute
+# Process identifiers of nodal attribute in target statistics
 tar_stats$attr$rural <- tar_stats$attr$rural %>% rownames_to_column(var = "node_id") %>% mutate(node_id = paste0("node_", node_id))
 tar_stats$attr$urban <- tar_stats$attr$urban %>% rownames_to_column(var = "node_id") %>% mutate(node_id = paste0("node_", node_id))
 
@@ -56,128 +56,115 @@ source("./R/result_helper_functions.R")
 
 
 # Process FRP data
-denom_r <-   n_r
-denom_u <- n_u
-
-frp_length_h_r <-
-  frp_length_df_process(
-    attr = tar_stats$attr$rural, 
-    frp_length = frp_h_r$lengths
-  )
+# frp_length_h_r <-
+#   frp_length_df_process(
+#     attr = tar_stats$attr$rural, 
+#     frp_length = frp_h_r$lengths,
+#     denom = n_r
+#   )
 
 frp_length_s_r <-
   frp_length_df_process(
     attr = tar_stats$attr$rural, 
     frp_length = frp_s_r$lengths,
-    denom = denom_r
+    denom = n_r
   )
 
 frp_length_w_r <-
   frp_length_df_process(
     attr = tar_stats$attr$rural, 
     frp_length = frp_w_r$lengths,
-    denom = denom_r
+    denom = n_r
   )
 
 frp_length_nh_r <-
   frp_length_df_process(
     attr = tar_stats$attr$rural, 
     frp_length = frp_nh_r$lengths,
-    denom = denom_r
+    denom = n_r
   )
 
 
 
+frp_length_s_u <-
+  frp_length_df_process(
+    attr = tar_stats$attr$urban, 
+    frp_length = frp_s_u$lengths,
+    denom = n_u
+  )
 
+frp_length_w_u <-
+  frp_length_df_process(
+    attr = tar_stats$attr$urban, 
+    frp_length = frp_w_u$lengths,
+    denom = n_u
+  )
 
+# Remove large outputs
+rm(list=
+     c(paste0("frp_", c("all", "h", "s", "w", "nh"), "_r"),
+       paste0("frp_", c("all", "h", "s", "w", "nh"), "_u")
+     )
+   )
 
 # Visualize FRP length
 ggarrange(
   frp_length_plot(frp_length =frp_length_s_r, title = "School, rural"),
   frp_length_plot(frp_length =frp_length_w_r, title = "Work, rural"),
   frp_length_plot(frp_length =frp_length_nh_r, title = "Nonhome, rural"),
-  ncol = 2, nrow = 2,
+  frp_length_plot(frp_length =frp_length_s_u, title = "School, urban"),
+  frp_length_plot(frp_length =frp_length_w_u, title = "Work, urban"),
+  ncol = 3, nrow = 2,
   common.legend = TRUE, 
   legend = "bottom"
 )
 
 
-
-
-# FRP interpretation
+# FRP length per n people at different time steps
 rbind(
-  #frp_moments_layer(frp_length_layer = frp_length_all_r, layer = layers[1]),
-  frp_moments_layer(frp_length_layer = frp_length_s_r, layer = "rural_school"),
-  frp_moments_layer(frp_length_layer = frp_length_w_r, layer = "rural_work"),
-  frp_moments_layer(frp_length_layer = frp_length_nh_r, layer = "rural_nonhome")
-
-)
-
-
-
-
-
-# Define a function to round character values within a data frame
-form_stats <- function(tar_stats, summary_stats){
-  
-round_df <- function(df) {
-  # Define a helper function to round character values
-  round_char_values <- function(value) {
-    if (grepl("\\(", value)) {
-      # Extract numbers from the value
-      numbers <- unlist(regmatches(value, gregexpr("[0-9.]+", value)))
-      # Round each number to 3 digits
-      rounded_numbers <- sapply(numbers, function(x) sprintf("%.3f", as.numeric(x)))
-      # Reconstruct the value with rounded numbers
-      rounded_value <- paste(rounded_numbers[1], "(", rounded_numbers[2], ")", sep = "")
-      return(rounded_value)
-    } else if (grepl("^[0-9.]+$", value)) {
-      return(sprintf("%.3f", as.numeric(value)))
-    } else {
-      return(value)
-    }
-  }
-  
-  # Apply the helper function to character columns in the data frame
-  df %>% mutate(across(where(is.character), ~sapply(.x, round_char_values)))
-}
+  frp_moments_layer(frp_length_layer = frp_length_s_u, per_n_people = 10000, layer = "urban_school"),
+  frp_moments_layer(frp_length_layer = frp_length_w_u, per_n_people = 10000, layer = "urban_work"),
+  frp_moments_layer(frp_length_layer = frp_length_s_r, per_n_people = 10000, layer = "rural_school"),
+  frp_moments_layer(frp_length_layer = frp_length_w_r, per_n_people = 10000, layer = "rural_work"),
+  frp_moments_layer(frp_length_layer = frp_length_nh_r, per_n_people = 10000, layer = "rural_nonhome")
+) %>% 
+  kbl() %>% 
+  kable_classic()
 
 
-## mean degree, converted from "edge" target statistics
+# One-year FRP by age
+## rural
+cbind(
+frp_moments_365_layer(frp_length_layer = frp_length_s_r, per_n_people = 10000),
+frp_moments_365_layer(frp_length_layer = frp_length_w_r, per_n_people = 10000) %>% select(-node.age.grp),
+frp_moments_365_layer(frp_length_layer = frp_length_nh_r, per_n_people = 10000) %>% select(-node.age.grp)
+)%>%  column_to_rownames(var = "node.age.grp") %>%
+  kbl() %>% 
+  kable_classic() %>%
+  add_header_above(c(" " = 1, "School, rural" = 3, "Work, rural" = 3, "Nonhome, rural" = 3))
 
-summary_stat_tb <- 
-c(
-tar_stats$targetstats_age.grp$formation_stats_rural$edge_ct_matrix$Home %>% sum()/n_r,
-paste0(tar_stats$targetstats_age.grp$formation_stats_rural$edge_ct_matrix$School %>% sum()/n_r, " (",
-       summary_stats$formation$formation_stats_rural$layer_assoc_rural$mean_deg_1day %>% filter(association == "s_by_w")%>% pull(other_layer.1), ")"
-       ),
-paste0(
-tar_stats$targetstats_age.grp$formation_stats_rural$edge_ct_matrix$Work %>% sum()/n_r, " (",
-summary_stats$formation$formation_stats_rural$layer_assoc_rural$mean_deg_1day %>% filter(association == "w_by_s") %>% pull(other_layer.1), ")"
-),
-tar_stats$targetstats_age.grp$formation_stats_rural$edge_ct_matrix$Nonhome %>% sum()/n_r
-) %>% cbind(., 
-c(
-  tar_stats$targetstats_age.grp$formation_stats_urban$edge_ct_matrix$Home %>% sum()/n_r,
-  paste0(tar_stats$targetstats_age.grp$formation_stats_urban$edge_ct_matrix$School %>% sum()/n_r, " (",
-         summary_stats$formation$formation_stats_urban$layer_assoc_urban$mean_deg_1day %>% filter(association == "s_by_w")%>% pull(other_layer.1), ")"
-  ),
-  paste0(
-    tar_stats$targetstats_age.grp$formation_stats_urban$edge_ct_matrix$Work %>% sum()/n_r, " (",
-    summary_stats$formation$formation_stats_urban$layer_assoc_urban$mean_deg_1day %>% filter(association == "w_by_s") %>% pull(other_layer.1), ")"
-  ),
-  tar_stats$targetstats_age.grp$formation_stats_urban$edge_ct_matrix$Nonhome %>% sum()/n_r
-),
-layers[-1]
-) %>% data.frame() %>% round_df(df=.) %>% select(3,1,2) %>% rename(layer=1, `rural`=2, `urban`=3)
+## urban
+cbind(
+  frp_moments_365_layer(frp_length_layer = frp_length_s_u, per_n_people = 10000),
+  frp_moments_365_layer(frp_length_layer = frp_length_w_u, per_n_people = 10000) %>% select(-node.age.grp)
+)%>%  column_to_rownames(var = "node.age.grp") %>%
+  kbl() %>% 
+  kable_classic() %>%
+  add_header_above(c(" " = 1, "School, urban" = 3, "Work, urban" = 3))
 
-summary_stat_tb
-}
 
-form_stats(tar_stats, summary_stats)
-
-## duration
-summary_stats$dissolution %>% pivot_wider(names_from = study_site, values_from = know_contact_duration) 
+# network statistics
+cbind(
+## formation
+form_stats(tar_stats, summary_stats),
+## dissolution
+summary_stats$dissolution %>% 
+  mutate(know_contact_duration = format(know_contact_duration, scientific = T, digits = 2)) %>% 
+  pivot_wider(names_from = study_site, values_from = know_contact_duration) %>% select(-contact_location)
+)%>%  column_to_rownames(var = "layer")%>%
+  kbl() %>% 
+  kable_classic() %>%
+  add_header_above(c(" " = 1,  "Mean degree (conditioned mean degree)" = 2, "Duration" = 2))
 ### Interpretation - as expected, the mean degree of the layer in the rural work layer is lower than urban.
 ### The contact duration is longer (weaker dissolvability), which may contributes to the lower FRP.
 ### The mean deg at work, conditioned on school having contact, is much lower, at the rural layer - this is the primary
--- R/result_helper_functions.R
+++ R/result_helper_functions.R
@@ -31,7 +31,7 @@ frp_length_plot <-
       pivot_longer(cols = starts_with("step_"), names_to = "time", values_to = "frp_length") %>%
       mutate(time = as.numeric(gsub("step_", "", time))) %>% 
       ggplot(aes(x = time, y = frp_length * 100, group = node_id, color = node.age.grp)) + # Multiply frp_length by 100
-      geom_line(size = 0.2, alpha = 0.6) +
+      geom_line(linewidth = 0.2, alpha = 0.6) +
       scale_color_manual(values = color_mapping) +
       scale_y_continuous(labels = function(x) percent_format(x)) + # Format y-axis using custom function
       labs(title = title, x = "", y = "FRP length/N") + # Update y-axis label
@@ -41,57 +41,157 @@ frp_length_plot <-
 
 
 # table showing statistical moments of FRP length in %
-frp_moments_layer <- function(frp_length_layer, layer){
-  
-  # Define the function to calculate mode
-  calculate_mode <- function(numbers) {
-    freq_table <- table(numbers)
-    mode_value <- as.numeric(names(freq_table)[which.max(freq_table)])
-    mode_frequency <- as.numeric(max(freq_table))
-    return(c(value = mode_value, frequency = mode_frequency))
-  }
-  
+frp_moments_layer <-
+  function(frp_length_layer, per_n_people, layer, d_365){
+
+  frp_length_layer <- frp_length_layer %>%
+    mutate(across(where(is.numeric), ~ . * per_n_people)) # multiple per_n_people to proportion to get the value per n people
+
   frp_moments <- function(numbers, scenario) {
-    # Convert to percentages and round appropriately
-    numbers <- numbers * 100
+    # Define the function to calculate mode
+    calculate_mode <- function(numbers) {
+      freq_table <- table(numbers)
+      mode_value <- as.numeric(names(freq_table)[which.max(freq_table)])
+      mode_frequency <- as.numeric(max(freq_table))
+      return(c(value = mode_value, frequency = mode_frequency))
+    }
+
     mean_value <- mean(numbers) %>% round(., 2)
     median_value <- median(numbers) %>% round(., 2)
     mode_result <- calculate_mode(as.numeric(numbers))
     mode_value <- mode_result["value"] %>% round(., 2)
-    mode_frequency <- mode_result["frequency"] 
+    mode_frequency <- mode_result["frequency"]
     iqr_value <- IQR(numbers) %>% round(., 2)
     min_value <- min(numbers) %>% round(., 2)
     max_value <- max(numbers) %>% round(., 2)
     q1_value <- quantile(numbers, 0.25) %>% round(., 2)
     q3_value <- quantile(numbers, 0.75) %>% round(., 2)
     min_frequency <- sum(numbers == min(numbers))
-    
+
     # Create a dataframe with the results
     result <- data.frame(
-      `Minimum (n)` = paste0(min_value, " (", min_frequency, ")"), 
-      Mean = paste0(mean_value, "%"), 
-      Median = paste0(median_value, "%"), 
+      `Minimum (n)` = paste0(min_value, " (", min_frequency, ")"),
+      Mean = mean_value,
+      Median = median_value,
       `Mode (n)` = paste0(mode_value, " (", mode_frequency, ")"),
-      Maximum = paste0(max_value, "%"), 
-      Q1 = paste0(q1_value, "%"), 
-      Q3 = paste0(q3_value, "%"),
-      IQR = paste0(iqr_value, "%"),
+      Maximum = max_value,
+      Q1 = q1_value,
+      Q3 = q3_value,
+      IQR = iqr_value,
       check.names = FALSE
     )
     rownames(result) <- scenario
-    
+
     return(result)
   }
-  
+
   ### Number of nodes with different FRP lengths at specific time points
   t <- c(0, seq(from = 1, to = 365, length.out = 3))
   t_name <- paste0("step_", t)
   scenario <- paste0(layer, "_", "step_", t)
-  
+
   rbind(
     frp_moments(numbers = frp_length_layer[, t_name[1]], scenario = scenario[1]), # t=0
     frp_moments(numbers = frp_length_layer[, t_name[2]], scenario = scenario[2]), # t=1
     frp_moments(numbers = frp_length_layer[, t_name[3]], scenario = scenario[3]), # t=183
     frp_moments(numbers = frp_length_layer[, t_name[4]], scenario = scenario[4])  # t=365
   )
+  }
+
+
+frp_moments_365_layer <-
+  function(frp_length_layer, per_n_people){
+    
+    frp_length_layer <- 
+      frp_length_layer %>%
+      mutate(across(where(is.numeric), ~ . * per_n_people)) # multiple per_n_people to proportion to get the value per n people
+    
+    summary_overall <- 
+     frp_length_layer %>% summarize(Mean =mean(step_365) %>% round(.,2), 
+                                   Median =median(step_365) %>% round(.,2), 
+                                   IQR = paste0(
+                                     quantile(step_365, 0.25)%>% round(.,2), ", ",
+                                     quantile(step_365, 0.75)%>% round(.,2)
+                                     )
+                                   ) %>% 
+      as.data.frame() %>% mutate(node.age.grp = "Overall") %>% select(4,1:3)
+    
+    summary_age_spec <- 
+    frp_length_layer %>% 
+      group_by(node.age.grp) %>% 
+      summarize(Mean =mean(step_365) %>% round(.,2), 
+                                   Median =median(step_365) %>% round(.,2), 
+                                   IQR = paste0(
+                                     quantile(step_365, 0.25)%>% round(.,2), ", ",
+                                     quantile(step_365, 0.75)%>% round(.,2)
+                                   ))%>% 
+      as.data.frame()
+    
+    rbind(
+      summary_overall, summary_age_spec
+    )
+  
+  }
+
+
+# Define a function to summarize formation statistics
+form_stats <- function(tar_stats, summary_stats){
+  
+  round_df <- function(df) {
+    # Define a helper function to round character values
+    round_char_values <- function(value) {
+      if (grepl("\\(", value)) {
+        # Extract numbers from the value
+        numbers <- unlist(regmatches(value, gregexpr("[0-9.]+", value)))
+        # Round each number to 3 digits
+        rounded_numbers <- sapply(numbers, function(x) sprintf("%.3f", as.numeric(x)))
+        # Reconstruct the value with rounded numbers
+        rounded_value <- paste(rounded_numbers[1], "(", rounded_numbers[2], ")", sep = "")
+        return(rounded_value)
+      } else if (grepl("^[0-9.]+$", value)) {
+        return(sprintf("%.3f", as.numeric(value)))
+      } else {
+        return(value)
+      }
+    }
+    
+    # Apply the helper function to character columns in the data frame
+    df %>% mutate(across(where(is.character), ~sapply(.x, round_char_values)))
+  }
+  
+  
+  ## mean degree, converted from "edge" target statistics
+  
+  summary_stat_tb <- 
+    c(
+      tar_stats$targetstats_age.grp$formation_stats_rural$edge_ct_matrix$Home %>% sum()/n_r,
+      paste0(tar_stats$targetstats_age.grp$formation_stats_rural$edge_ct_matrix$School %>% sum()/n_r, " (",
+             summary_stats$formation$formation_stats_rural$layer_assoc_rural$mean_deg_1day %>% filter(association == "s_by_w")%>% pull(other_layer.1), ")"
+      ),
+      paste0(
+        tar_stats$targetstats_age.grp$formation_stats_rural$edge_ct_matrix$Work %>% sum()/n_r, " (",
+        summary_stats$formation$formation_stats_rural$layer_assoc_rural$mean_deg_1day %>% filter(association == "w_by_s") %>% pull(other_layer.1), ")"
+      ),
+      tar_stats$targetstats_age.grp$formation_stats_rural$edge_ct_matrix$Nonhome %>% sum()/n_r
+    ) %>% cbind(., 
+                c(
+                  tar_stats$targetstats_age.grp$formation_stats_urban$edge_ct_matrix$Home %>% sum()/n_r,
+                  paste0(tar_stats$targetstats_age.grp$formation_stats_urban$edge_ct_matrix$School %>% sum()/n_r, " (",
+                         summary_stats$formation$formation_stats_urban$layer_assoc_urban$mean_deg_1day %>% filter(association == "s_by_w")%>% pull(other_layer.1), ")"
+                  ),
+                  paste0(
+                    tar_stats$targetstats_age.grp$formation_stats_urban$edge_ct_matrix$Work %>% sum()/n_r, " (",
+                    summary_stats$formation$formation_stats_urban$layer_assoc_urban$mean_deg_1day %>% filter(association == "w_by_s") %>% pull(other_layer.1), ")"
+                  ),
+                  tar_stats$targetstats_age.grp$formation_stats_urban$edge_ct_matrix$Nonhome %>% sum()/n_r
+                ),
+                layers[-1]
+    ) %>% data.frame() %>% round_df(df=.) %>% select(3,1,2) %>% rename(layer=1, `Rural`=2, `Urban`=3)
+  
+  summary_stat_tb
 }
+
+
+
+
+
