e573c49bf75c02c21aed099dd50c47f26e119791
billydehaochen <dehaochenbilly@gmail.com>
2024-12-10 14:05:39 -0500

    Update workflow-netdx.R

--- R/workflow-netdx.R
+++ R/workflow-netdx.R
@@ -13,7 +13,7 @@ hpc_context <- TRUE
 source("R/hpc_configs.R", local = TRUE)
 
 # Process ----------------------------------------------------------------------
-wf <- make_em_workflow("netdx_0.1_u_1210", override = TRUE)
+wf <- make_em_workflow("netdx_0.1_r_1210", override = TRUE)
 
 
 # netdx
@@ -24,7 +24,7 @@ wf <- add_workflow_step(
     layer=c("School", "Work", "Nonhome"),
     MoreArgs = list(
       hpc_context = TRUE,
-      network=c("Urban"),
+      network=c("Rural"),
       est_apch="mcmle",
       percent_target_pop="0.1"),
     setup_lines = hpc_node_setup
