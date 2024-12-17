4b6ab89b02f24402d71ffd73927709467607d783
billydehaochen <dehaochenbilly@gmail.com>
2024-12-17 11:11:07 -0500

    Update workflow-netsim_tsna.R

--- R/workflow-netsim_tsna.R
+++ R/workflow-netsim_tsna.R
@@ -10,7 +10,7 @@ hpc_context <- TRUE
 source("R/hpc_configs.R", local = TRUE)
 
 # Process ----------------------------------------------------------------------
-wf <- make_em_workflow("netsim_frp_h_all_u_0.1_1211", override = TRUE)
+wf <- make_em_workflow("netsim_frp_r_1_1216", override = TRUE)
 
 
 # netsim
@@ -19,14 +19,14 @@ wf <- add_workflow_step(
   step_tmpl = step_tmpl_do_call_script(
     r_script = "R/5-network_sim.R",
     args = list(hpc_context = TRUE,
-                network="Urban",
+                network="Rural",
                 est_apch="mcmle",
-                percent_target_pop="0.1"),
+                percent_target_pop="1"),
     setup_lines = hpc_node_setup
   ),
   sbatch_opts = list(
     "cpus-per-task" = est_cores,
-    "time" = "36:00:00",
+    "time" = "120:00:00",
     "mem" = "0"
   )
 )
@@ -36,17 +36,17 @@ wf <- add_workflow_step(
   wf_summary = wf,
   step_tmpl = step_tmpl_map_script(
     r_script = "R/6-tsna.R",
-    layer=c("All", "Home"),
+    layer=c("All", "Home", "School", "Work", "Nonhome"),
     MoreArgs = list(hpc_context = TRUE,
-                    network="Urban",
+                    network="Rural",
                     est_apch="mcmle",
-                    percent_target_pop="0.1",
+                    percent_target_pop="1",
                     nodes=NULL), 
     setup_lines = hpc_node_setup
   ),
   sbatch_opts = list(
     "cpus-per-task" = est_cores,
-    "time" = "36:00:00",
+    "time" = "120:00:00",
     "mem" = "0"
   )
 )
