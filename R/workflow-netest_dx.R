4c232ead311cc0e5519a53aa1ad84c3b43146c0e
billydehaochen <dehaochenbilly@gmail.com>
2025-03-05 13:28:02 -0500

    push

--- R/workflow-netest_dx.R
+++ R/workflow-netest_dx.R
@@ -8,20 +8,20 @@ hpc_context <- TRUE
 source("R/hpc_configs.R", local = TRUE)
 
 # Process ----------------------------------------------------------------------
-wf <- make_em_workflow("netest_dx_0.5_nh_u_0225", override = TRUE)
+wf <- make_em_workflow("netest_dx_0.1_r_0304", override = TRUE)
 
 # netest
 wf <- add_workflow_step(
   wf_summary = wf,
   step_tmpl = step_tmpl_map_script(
     r_script = "R/3-network_est.R",
-    layer=c( "Nonhome"),
+    layer=c("School", "Work", "Nonhome"),
     
     MoreArgs = list(
                 hpc_context = TRUE,
-                network=c("Urban"),
+                network=c("Rural"),
                 est_apch="mcmle",
-                percent_target_pop="0.5"),
+                percent_target_pop="0.1"),
     setup_lines = hpc_node_setup
   ),
   sbatch_opts = list(
@@ -36,12 +36,12 @@ wf <- add_workflow_step(
   wf_summary = wf,
   step_tmpl = step_tmpl_map_script(
     r_script = "R/4-network_dx.R",
-    layer=c( "Nonhome"),
+    layer=c("School", "Work", "Nonhome"),
     MoreArgs = list(
       hpc_context = TRUE,
-      network=c("Urban"),
+      network=c("Rural"),
       est_apch="mcmle",
-      percent_target_pop="0.5"),
+      percent_target_pop="0.1"),
     setup_lines = hpc_node_setup
   ),
   sbatch_opts = list(
